439f3841a2df664c9b396456292521c08c555ac8
StanleyY7 <stanleyyu7@gmail.com>
2023-03-24 22:32:44 +1100

    media query touch ups

--- Legion/src/containers/CartPage/CartPage.jsx
+++ Legion/src/containers/CartPage/CartPage.jsx
@@ -1,18 +1,17 @@
 import SocialSection from "../../components/SocialSection/SocialSection";
 
-
 import { startPageAtTop } from "../../Services/general";
 import CartList from "../../components/CartList/CartList";
-
+import styles from "../CartPage/CartPage.module.scss";
 const CartPage = () => {
-
-
   startPageAtTop();
 
   return (
     <>
-      <CartList/>
-        <SocialSection />      
+      <CartList />
+      <div className={styles.endCartWrapper}>
+        <SocialSection />
+      </div>
     </>
   );
 };
